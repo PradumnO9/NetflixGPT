746f9dad624dddc5acf5723f0a5d474681952dba
PradumnO9 <pradumn2999@gmail.com>
2024-07-20 23:04:41 +0530

    fix env

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -8,7 +8,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTFmZTNlNGRlZDAxODU1MDUzNTcyMzkwZjNhODNkZiIsIm5iZiI6MTcyMDYwOTU5Ni41MjY2NzQsInN1YiI6IjY2OGU2ODRkM2JhZWJlZWVkZDllZDQ2YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XenVvX2MKlseWT8YPetwJZ0-ay3gV7Ywqgx5wQfuWI4",
   },
 };
 
@@ -17,10 +17,12 @@ export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
 export const BG_IMG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg";
 
-export const SUPPORTED_LANGUAGES = [
+export const SUPPORTED_LANGUAGES = [ 
   { identifier: "en", name: "English" },
   { identifier: "hindi", name: "Hindi" },
   { identifier: "spanish", name: "Spanish" },
 ];
 
-export const OPEN_AI_KEY = process.env.REACT_APP_OPEN_AI_KEY;
+// export const OPEN_AI_KEY = process.env.REACT_APP_OPEN_AI_KEY;
+
+export const OPEN_AI_KEY = "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";
